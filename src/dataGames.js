7346e5d89aea93d49f557aff08e1da98331c6fb7
akbar-kurnia1 <akbarkurniawan790@gmail.com>
2026-03-27 18:08:14 +0700

    feat: add game list yeaa

---
+++ src/dataGames.js
@@ -0,0 +1,22 @@
+export const gameList=[
+    {
+        id: 1,
+        name: 'Ace Combat 7: Skies Unknown',
+        genre: 'Flight Simulation',
+    },
+    {
+        id: 2,
+        name: 'Stardew Valley',
+        genre: 'Simulation',
+    },
+    {
+        id: 3,
+        name: 'Left 4 Dead 2',
+        genre: 'First-Person Shooter',
+    },
+    {
+        id: 4,
+        name: "Call of Duty: Modern Warfare",
+        genre: 'First-Person Shooter',
+    }
+]
