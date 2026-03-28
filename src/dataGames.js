20233325399e5663cf48997d1c522be96205b7cd
akbar-kurnia1 <akbarkurniawan790@gmail.com>
2026-03-28 23:16:47 +0700

    feat: add cover images and update game list

--- src/dataGames.js
+++ src/dataGames.js
@@ -1,22 +1,59 @@
+import coverAceCombat from './assets/coverAceCombat.jpg';
+import coverStardewValley from './assets/coverStardewValley.jpg';
+import coverLeft4Dead2 from './assets/coverLeft4Dead2.jpg';
+import coverCallOfDuty from './assets/coverCallOfDuty.jpg';
+import coverCOH from './assets/coverCOH.jpg';
+import coverMetro2033 from './assets/coverMetro2033.jpg';
+import coverMetroLastLight from './assets/coverMetroLastLight.jpg';
+import coverGTA from './assets/coverGTA.jpg';
+
 export const gameList=[
     {
         id: 1,
         name: 'Ace Combat 7: Skies Unknown',
         genre: 'Flight Simulation',
+        cover: coverAceCombat,
     },
     {
         id: 2,
         name: 'Stardew Valley',
         genre: 'Simulation',
+        cover: coverStardewValley,
     },
     {
         id: 3,
         name: 'Left 4 Dead 2',
         genre: 'First-Person Shooter',
+        cover: coverLeft4Dead2,
     },
     {
         id: 4,
-        name: "Call of Duty: Modern Warfare",
+        name: "Call of Duty: Modern Warfare 2",
         genre: 'First-Person Shooter',
-    }
+        cover: coverCallOfDuty,
+    },
+    {
+        id: 5,
+        name: "Company of Heroes",
+        genre: 'Real-Time Strategy',
+        cover: coverCOH,
+    },
+    {
+        id: 6,
+        name: "Metro 2033 Redux",
+        genre: 'First-Person Shooter',
+        cover: coverMetro2033,
+    },
+    {
+        id: 7,
+        name: "Metro: Last Light Redux",
+        genre: 'First-Person Shooter',
+        cover: coverMetroLastLight,
+    },
+    {
+        id: 8,
+        name: "GTA San Andreas",
+        genre: 'Action-Adventure',
+        cover: coverGTA,
+    },
 ]
